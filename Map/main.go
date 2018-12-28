a4c9af235fb4ce908b171560b9b640fe86d386c4
rbenites <rbenites115@gmail.com>
2018-12-28 11:29:32 -0800

    - added a way to iterate through the key value pairs in the map

--- Map/main.go
+++ Map/main.go
@@ -6,6 +6,14 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#008000",
+		"white": "#ffff",
 	}
-	fmt.Println(colors)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
