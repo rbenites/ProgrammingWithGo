4518134035e76570c9a580064b6ba2e17ecbbaf0
rbenites <rbenites115@gmail.com>
2019-01-14 09:12:03 -0800

    - completed jedi2 ex4

--- ToddMcLeod_Course/exercises/jedi2/exercise4/main.go
+++ ToddMcLeod_Course/exercises/jedi2/exercise4/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main(){}
+import "fmt"
+
+/*
+- assign an int to a variable
+- print the int in decimal, binary, and hex
+- shift the bits of the int over 1 position to the left and assin that to a variable
+- print the int in decimal, binary, and hex
+*/
+
+func main() {
+	a := 2
+	fmt.Printf("%d\t%b\t%#x\n", a, a, a)
+	b := a << 1
+	fmt.Printf("%d\t%b\t%#x\n", b, b, b)
+
+}
