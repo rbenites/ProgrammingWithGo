7409f2ab8c96cd11e507edb8f52d1bd0f70c0cbe
rbenites <rbenites115@gmail.com>
2019-01-14 09:17:37 -0800

    - completed jedi2 ex5

--- ToddMcLeod_Course/exercises/jedi2/exercise5/main.go
+++ ToddMcLeod_Course/exercises/jedi2/exercise5/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main(){}
+import "fmt"
+
+/*
+- create a variable of type string using raw string literal
+- print the variable
+*/
+
+func main() {
+	a := `here is something
+as
+a
+raw string
+literal "you see"`
+	fmt.Println(a)
+
+}
