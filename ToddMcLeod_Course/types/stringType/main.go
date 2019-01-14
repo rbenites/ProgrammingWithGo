2c85d492c887e10bdd933aa327f2ba285d2db771
rbenites <rbenites115@gmail.com>
2019-01-13 21:36:34 -0800

    - updated stringType

--- ToddMcLeod_Course/types/stringType/main.go
+++ ToddMcLeod_Course/types/stringType/main.go
@@ -1,4 +1,5 @@
 package main
+
 //byte = alias for int8
 
 import "fmt"
@@ -17,8 +18,23 @@ func main() {
 	fmt.Printf("%T\n", u)
 
 	//slice of byte. convert s to int
-	bs:=[]byte(s)
+	bs := []byte(s)
 	fmt.Println(bs)
 	fmt.Printf("%T\n", bs)
 
+	//converts each character into UTF8
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%#U", s[i])
+	}
+	fmt.Println("")
+
+	//print index  position and the int conversion of the string
+	for i, v := range s {
+		fmt.Println(i, v)
+	}
+
+	//print the index position of the string and the hex decimal
+	for i, v := range s {
+		fmt.Printf("at index position %d we have %#x\n", i, v)
+	}
 }
