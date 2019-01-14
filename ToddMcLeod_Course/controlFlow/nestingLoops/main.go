4a3b752318cf935e6915c6ef1cd2ac372c95efb5
rbenites <rbenites115@gmail.com>
2019-01-14 11:52:16 -0800

    - added more code - added comments

--- ToddMcLeod_Course/controlFlow/nestingLoops/main.go
+++ ToddMcLeod_Course/controlFlow/nestingLoops/main.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	//for statement with for clause (nested for loop)
+	//for intit; condition; post {}
 	for i := 0; i <= 10; i++ {
 		fmt.Printf("The outer loop: %d\n ", i)
 		for j := 0; j < 3; j++ {
 			fmt.Printf("\t\t The inner loop: %d\n", j)
 		}
 	}
+
+	//for statement with single clause
+	x := 1
+	for x < 10 {
+		fmt.Println(x)
+		x++
+	}
+	fmt.Println("done.")
 
 }
