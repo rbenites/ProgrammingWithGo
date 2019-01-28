c63308b979ca948dc46dd9676c01b77b96ce25fc
rbenites <rbenites115@gmail.com>
2019-01-28 14:22:11 -0800

    - completed ex5

--- ToddMcLeod_Course/exercises/jedi4/exercise5/main.go
+++ ToddMcLeod_Course/exercises/jedi4/exercise5/main.go
@@ -1,7 +1,18 @@
 package main
 
-/**/
+import "fmt"
+
+/*
+- Using the following slice:
+	- x:=[]int{42,43,44,45,46,47,48,49,50,51}
+	use APPEND and SLICING to get the following values:
+		-[42, 43, 44, 45, 46, 48, 49, 50]
+	- print it out
+
+*/
 
 func main() {
-
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	x = append(x[:5], x[6:9]...)
+	fmt.Println(x)
 }
