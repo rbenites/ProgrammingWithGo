3fd456fd10654e033ba053c7edcc2924227dc96f
rbenites <rbenites115@gmail.com>
2019-01-28 14:03:23 -0800

    - completed ex4

--- ToddMcLeod_Course/exercises/jedi4/exercise4/main.go
+++ ToddMcLeod_Course/exercises/jedi4/exercise4/main.go
@@ -1,7 +1,29 @@
 package main
 
-/**/
+import "fmt"
+
+/*
+- start with the following slice:
+	- x:=[]int{42,43,44,45,46,47,48,49,50,51}
+- append to the the slice the following value:
+	- 52
+- in ONE STATEMENT append to the slice the following values:
+	- 53
+	- 54
+	- 55
+- print out the slice
+- append to the slice the following slice
+	- x:=[]int{56,57,58,59,60}
+print out the slice
+*/
 
 func main() {
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	x = append(x, 52)
+	fmt.Println(x)
+	x = append(x, 53, 54, 55)
+	fmt.Println(x)
+	x = append([]int{56, 57, 58, 59, 60})
+	fmt.Println(x)
 
 }
