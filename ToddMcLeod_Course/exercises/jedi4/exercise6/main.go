b91a0ec6f1eb2b09d4625327bbacfc88c04524a0
rbenites <rbenites115@gmail.com>
2019-01-28 15:20:10 -0800

    - completed ex7

--- ToddMcLeod_Course/exercises/jedi4/exercise6/main.go
+++ ToddMcLeod_Course/exercises/jedi4/exercise6/main.go
@@ -1,7 +1,45 @@
 package main
 
-/**/
+import "fmt"
+
+/*
+- creaeate a slice to store the names of all the states in the US
+- Print out the folloiwing:
+	- Length of the slice
+	- Capacity of the slice
+	- Values and their indexes
+- DO NOT USE RANGE
+*/
 
 func main() {
+	x := make([]string, 50, 50)
+	x = []string{` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`}
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	for i := 0; i < len(x); i++ {
+		fmt.Println(i, x[i])
+	}
+
+	//OR
+
+	y := make([]string, 50, 52)
+	fmt.Println(len(y), cap(y))
+	for i := 0; i < 50; i++ {
+		x[i] = fmt.Sprintf("Position %d", i)
+	}
+	fmt.Println(y)
+	fmt.Println(len(y))
+	fmt.Println(cap(y))
+
+	y = append(x, ` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`)
+	fmt.Println(y)
+	fmt.Println(len(y))
+	fmt.Println(cap(y))
+
+	for i := 0; i < len(y); i++ {
+		fmt.Println(i, y[i])
+	}
 
 }
--- ToddMcLeod_Course/exercises/jedi4/exercise7/main.go
+++ ToddMcLeod_Course/exercises/jedi4/exercise7/main.go
@@ -1,7 +1,29 @@
 package main
 
-/**/
+import "fmt"
+
+/*
+- Create a slice of a slice of string
+	-ex: [][]string
+	-Store the following
+		-"James","Bond","Shaken, not stirred"
+		-"Miss","Moneypenny","Helo, james"
+- Range over the records, then range over the data in each record
+*/
 
 func main() {
+	jb := []string{"James", "Bond", "Shaken, not stirred"}
+	fmt.Println(jb)
+	mp := []string{"Miss", "Moneypenny", "Helo, james"}
+	fmt.Println(mp)
 
+	ss := [][]string{jb, mp}
+	fmt.Println(ss)
+
+	for i, s := range ss {
+		fmt.Println("Record: ", i)
+		for j, val := range s {
+			fmt.Printf("\t index position: %v\t value: \t %v\n", j, val)
+		}
+	}
 }
