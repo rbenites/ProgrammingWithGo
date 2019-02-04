47c6b5db691eabfb4dfeba5e1542a8b2c72fbcf8
rbenites <rbenites115@gmail.com>
2019-02-04 15:47:26 -0800

    - completed Jedi5 ex3

--- ToddMcLeod_Course/exercises/jedi5/exercise3/main.go
+++ ToddMcLeod_Course/exercises/jedi5/exercise3/main.go
@@ -1,4 +1,7 @@
 package main
+
+import "fmt"
+
 /*
 - Create new type: VEHICLE
 	- underlying type is a struct
@@ -9,11 +12,43 @@ package main
 	- The underlying type of each of these new type is a struct
 	- Embed the "vehicle" type in both truck & sedan
 	- Give truck the field "fourWheel" which will be set to bool.
-	- Give the dedan the filed "luxury" which will be set to bool.
+	- Give the dedan the field "luxury" which will be set to bool.
 - Using the VEHICLE, TRUCK, and SEDAN structs
 	- use a comparison literal, create a value of type truck and assign values to the fields
 	-Using composite literal, create a value of type sedan and assign values to the fields.
 
 */
 
-func main(){}
+type vehicle struct {
+	doors int
+	color string
+}
+
+type truck struct {
+	vehicle
+	fourwheel bool
+}
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+func main() {
+	t := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "white",
+		},
+		fourwheel: true,
+	}
+	fmt.Println(t)
+
+	s := sedan{
+		vehicle: vehicle{
+			doors: 4,
+			color: "black",
+		},
+		luxury: false,
+	}
+	fmt.Println(s)
+}
--- ToddMcLeod_Course/exercises/jedi5/exercise4/main.go
+++ ToddMcLeod_Course/exercises/jedi5/exercise4/main.go
@@ -1,3 +1,9 @@
 package main
 
-func main(){}
+/*
+- Create and use an anon struct
+*/
+
+func main() {
+
+}
