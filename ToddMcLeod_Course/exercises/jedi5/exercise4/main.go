ea4511031930ee911118435ae9798fa0ec48e1c9
rbenites <rbenites115@gmail.com>
2019-02-05 14:46:40 -0800

    - completed jedi5/ex4 - deleted ex 5-7

--- ToddMcLeod_Course/exercises/jedi5/exercise4/main.go
+++ ToddMcLeod_Course/exercises/jedi5/exercise4/main.go
@@ -1,9 +1,36 @@
 package main
 
+import "fmt"
+
 /*
 - Create and use an anon struct
 */
 
 func main() {
+	s := struct {
+		first     string
+		friends   map[string]int
+		favDrinks []string
+	}{
+		first: "James",
+		friends: map[string]int{
+			"Moneypenny": 555,
+			"Q":          777,
+			"M":          888,
+		},
+		favDrinks: []string{
+			"Martini",
+			"water",
+		},
+	}
+	fmt.Println(s)
 
+	for k, v := range s.friends {
+		fmt.Println(k, v)
+
+	}
+
+	for i,val:=range s.favDrinks{
+		fmt.Println(i, val)
+	}
 }
